d1770735d7fe5e0fb308a94d91bd2f2332589586
Athisruth M Satheesh <athisruth@outlook.com>
2026-04-25 19:35:27 +0530

    rightsidebar added

--- anatomy-chat-app/src/components/ChatBox/ChatBox.jsx
+++ anatomy-chat-app/src/components/ChatBox/ChatBox.jsx
@@ -19,6 +19,14 @@ const ChatBox = () => {
                     </div>
                 </div>
 
+                <div className='s-msg'>
+                    <img className='msg-img' src={assets.pic1} alt="" />
+                    <div>
+                        <img src={assets.profile_img} alt="" />
+                        <p>2:30 PM</p>
+                    </div>
+                </div>
+
                 <div className='r-msg'>
                     <p className='msg'>Lorem ipsum is placeholder</p>
                     <div><img src={assets.profile_img} alt="" />
