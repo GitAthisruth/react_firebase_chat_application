6a0e0416ca21488e56afa7358f7c57ae710c41b4
Athisruth M Satheesh <athisruth@outlook.com>
2026-04-26 02:05:51 +0530

    profile updater

--- anatomy-chat-app/src/pages/ProfileUpdate/ProfileUpdate.jsx
+++ anatomy-chat-app/src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -1,9 +1,23 @@
 import React from "react";
+import assets from "../../assets/assets";
 import './ProfileUpdate.css';
 const ProfileUpdate = () => {
     return (
-        <div>
-            <h1>We are on the ProfileUpdate</h1>
+        <div className="profile">
+            <div className="profile-container">
+                <form>
+                    <h3>Profile details</h3>
+                    <label htmlFor="avatar">
+                        <input type="file" id="avatar" accept='.png,.jpeg,.jpg' hidden />
+                        <img src={assets.avatar_icon} alt="" />
+                        upload profile image
+                    </label>
+                    <input type="text" placeholder="Your Name" required />
+                    <textarea placeholder="write profile bio" required></textarea>
+                    <button type="submit">Save</button>
+                </form>
+                <img className="profile-pic" src={assets.logo_icon} alt="" />
+            </div>
         </div>
     );
 };
