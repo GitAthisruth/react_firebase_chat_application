9c34c98811e86c88f7c56e755e5d054138bd4e96
Athisruth M Satheesh <athisruth@outlook.com>
2026-04-26 02:18:34 +0530

    profile page updated

--- anatomy-chat-app/src/pages/ProfileUpdate/ProfileUpdate.jsx
+++ anatomy-chat-app/src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -1,22 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import assets from "../../assets/assets";
 import './ProfileUpdate.css';
 const ProfileUpdate = () => {
+
+    const [image, setImage] = useState(false);
+
     return (
         <div className="profile">
             <div className="profile-container">
                 <form>
                     <h3>Profile details</h3>
                     <label htmlFor="avatar">
-                        <input type="file" id="avatar" accept='.png,.jpeg,.jpg' hidden />
-                        <img src={assets.avatar_icon} alt="" />
+                        <input onChange={(e) => setImage(e.target.files[0])} type="file" id="avatar" accept='.png,.jpeg,.jpg' hidden />
+                        <img src={image ? URL.createObjectURL(image) : assets.avatar_icon} alt="" />
                         upload profile image
                     </label>
                     <input type="text" placeholder="Your Name" required />
                     <textarea placeholder="write profile bio" required></textarea>
                     <button type="submit">Save</button>
                 </form>
-                <img className="profile-pic" src={assets.logo_icon} alt="" />
+                <img className="profile-pic" src={image ? URL.createObjectURL(image) : assets.logo_icon} alt="" />
             </div>
         </div>
     );
