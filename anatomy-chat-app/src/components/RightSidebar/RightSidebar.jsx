a87b7ef3f86ad8823e0d8e4baa4c240300374196
Athisruth M Satheesh <athisruth@outlook.com>
2026-04-25 16:07:42 +0530

    leftsidebar created

---
+++ anatomy-chat-app/src/components/RightSidebar/RightSidebar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import './RightSidebar.css';
+const RightSidebar = () => {
+    return (
+        <div>
+        </div>
+    );
+}
+export default RightSidebar;
--- anatomy-chat-app/src/pages/Login/Login.jsx
+++ anatomy-chat-app/src/pages/Login/Login.jsx
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import './Login.css';
+import assets from "../../assets/assets";
+
 const Login = () => {
+
+    const [currentState, setCurrentState] = useState("Sign up");
+
     return (
-        <div>
-            <h1>We are on the Login page</h1>
+        <div className="login">
+            <img src={assets.logo_big} alt="" className="logo" />
+            <form className="login-form">
+                <h2>{currentState}</h2>
+                {currentState === "Sign up" ? <input type="text" placeholder="username" className="form-input" required /> : null}
+                <input type="email" placeholder="email address" className="form-input" required />
+                <input type="password" placeholder="password" className="form-input" required />
+                <button type="submit">{currentState === "Sign up" ? "Create account" : "Login now"}</button>
+                <div className="login-term">
+                    <input type="checkbox" />
+                    <p>Agree to the terms of use & privacy policy</p>
+                </div>
+                <div className="login-forgot">
+                    {
+                        currentState === "Sign up"
+
+                            ? <p className="login-toggle">Already have an account <span onClick={() => setCurrentState("Login")}>click here</span></p>
+                            : <p className="login-toggle">Create an account<span onClick={() => setCurrentState("Sign up")}>click here</span></p>
+                    }
+                </div>
+            </form>
         </div>
     );
 };
