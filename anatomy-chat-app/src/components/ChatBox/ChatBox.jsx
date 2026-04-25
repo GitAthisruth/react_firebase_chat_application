dcf7cf0833d96445306c922f89a85bce4d2b5814
Athisruth M Satheesh <athisruth@outlook.com>
2026-04-25 18:50:56 +0530

    div tag error fixed

--- anatomy-chat-app/src/components/ChatBox/ChatBox.jsx
+++ anatomy-chat-app/src/components/ChatBox/ChatBox.jsx
@@ -1,8 +1,42 @@
 import React from 'react';
 import './ChatBox.css';
+import assets from '../../assets/assets';
 const ChatBox = () => {
     return (
-        <div>
+        <div className='chat-box'>
+            <div className='chat-user'>
+                <img src={assets.profile_img} alt="" />
+                <p>Richard Sanford <img className='dot' src={assets.green_dot} alt="" /></p>
+                <img src={assets.help_icon} alt="" className='help' />
+            </div>
+
+            <div className="chat-msg">
+                <div className='s-msg'>
+                    <p className='msg'>Lorem ipsum is placeholder</p>
+                    <div>
+                        <img src={assets.profile_img} alt="" />
+                        <p>2:30 PM</p>
+                    </div>
+                </div>
+
+                <div className='r-msg'>
+                    <p className='msg'>Lorem ipsum is placeholder</p>
+                    <div><img src={assets.profile_img} alt="" />
+                        <p>2:30 PM</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className='chat-input'>
+                <input type="text" placeholder='Send a message' />
+                <input type="file" id='image' accept='image/png,image/jpeg' hidden />
+                <label htmlFor="image">
+                    <img src={assets.gallery_icon} alt="" />
+                </label>
+                <label htmlFor="image">
+                    <img src={assets.send_button} alt="" />
+                </label>
+            </div>
         </div>
     );
 }
