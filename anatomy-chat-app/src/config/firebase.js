fbed70768c3362a03885a3f03508cc32246c0d1c
Athisruth M Satheesh <athisruth@outlook.com>
2026-04-26 12:39:22 +0530

    firebase config added

---
+++ anatomy-chat-app/src/config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyD05z9JFVPltFAkoGqxeKW8XviVemC3gWo",
+    authDomain: "anatomy-lab-chat-test-app.firebaseapp.com",
+    projectId: "anatomy-lab-chat-test-app",
+    storageBucket: "anatomy-lab-chat-test-app.firebasestorage.app",
+    messagingSenderId: "14576108321",
+    appId: "1:14576108321:web:a8c0a0c7c01e1f62fd7460"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
